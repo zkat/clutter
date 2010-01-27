22d4e6122dddeff3824fd0842c73e7c883d34c84
Josh Marchán <sykopomp@sykosomatic.org>
2010-01-26 22:13:38 -0600

    Fuck the value cell.

--- src/symbols.lisp
+++ src/symbols.lisp
@@ -4,13 +4,16 @@
 
 (defstruct clutter-symbol
   "A Clutter symbol in the interpreted lisp"
-  name value)
+  name)
 
 (defvar *clutter-symbols* (make-hash-table :test #'equal)
   "Flat namespace of all clutter symbols")
 
+(defun clutter-intern (string)
+  (setf (gethash name *clutter-symbols*)
+        (make-clutter-symbol :name name)))
+
 (defun ensure-clutter-symbol (name)
   (check-type name string)
   (or (gethash name *clutter-symbols*)
-      (setf (gethash name *clutter-symbols*)
-            (make-clutter-symbol :name name))))
+      (clutter-intern name)))
