5aa73a4176d9f89659655be6a89044397028f723
Adlai Chandrasekhar <munchking@gmail.com>
2010-01-27 06:37:15 +0200

    single escapes for symbol names

--- src/reader.lisp
+++ src/reader.lisp
@@ -5,9 +5,13 @@
 (defvar *symbol-terminators*
   '(#\Space #\( #\) #\Newline))
 
+(defvar *symbol-single-escape* #\\)
+
 (defun read-clutter-symbol (stream)
   (do ((name (make-array 10 :element-type 'character :adjustable t :fill-pointer 0))
        (char (read-char stream) (read-char stream)))
       ((find char *symbol-terminators*)
        (ensure-clutter-symbol (coerce name 'simple-string)))
+    (when (eq char *symbol-single-escape*)
+      (setf char (read-char stream)))
     (vector-push-extend char name)))
