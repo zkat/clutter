efd16f831e11fcbace54aa36fc492c3297a8c4ad
Benjamin Saunders <besaunde@sfu.ca>
2010-07-06 00:16:31 -0700

    Test lexical binding.

--- test/environments.lisp
+++ test/environments.lisp
@@ -6,7 +6,13 @@
 
 (test stack-frame)
 (test *stack*)
-(test bind)
+(test binding
+  (let* ((ns (make-namespace))
+         (symbol (make-clutter-symbol :name "test" :namespace ns))) ;TODO: clutter-gensym to ensure no collision
+    (is (not (clutter-boundp symbol :lexical)))
+    (is (bind symbol "value" :lexical))
+    (is (clutter-boundp symbol :lexical))
+    (is-true (unbind symbol :lexical) "Failed to unbind the symbol.")))
 (test with-frame)
 (test push-initial-binding)
 (test push-initial-function-binding)
