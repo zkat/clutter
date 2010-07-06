335d5e96ad4df731beebe255baa7083152ce5857
Benjamin Saunders <besaunde@sfu.ca>
2010-07-06 00:18:17 -0700

    Expand binding test to cover all subenvs.

--- test/environments.lisp
+++ test/environments.lisp
@@ -7,12 +7,14 @@
 (test stack-frame)
 (test *stack*)
 (test binding
-  (let* ((ns (make-namespace))
-         (symbol (make-clutter-symbol :name "test" :namespace ns))) ;TODO: clutter-gensym to ensure no collision
-    (is (not (clutter-boundp symbol :lexical)))
-    (is (bind symbol "value" :lexical))
-    (is (clutter-boundp symbol :lexical))
-    (is-true (unbind symbol :lexical) "Failed to unbind the symbol.")))
+  (mapc #'(lambda (env)
+            (let* ((ns (make-namespace))
+                   (symbol (make-clutter-symbol :name "test" :namespace ns))) ;TODO: clutter-gensym to ensure no collision
+              (is (not (clutter-boundp symbol env)))
+              (is (bind symbol "value" env))
+              (is (clutter-boundp symbol env))
+              (is-true (unbind symbol env) "Failed to unbind ~A from ~A." symbol env)))
+        '(:function :lexical :dynamic :namespace)))
 (test with-frame)
 (test push-initial-binding)
 (test push-initial-function-binding)
