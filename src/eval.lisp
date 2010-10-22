74b7a9c1121b1658fa2a1bddef0984c9359fcfbe
Benjamin Saunders <besaunde@sfu.ca>
2010-10-22 13:16:01 -0700

    Made environments structs.

--- src/eval.lisp
+++ src/eval.lisp
@@ -3,6 +3,9 @@
 (defpackage #:fexpr-clutter (:use :cl :alexandria))
 (in-package #:fexpr-clutter)
 
+(defstruct env
+  parent bindings)
+
 (defun clutter-eval (expression environment)
   (cond ((symbolp expression) (eval/symbol expression environment))
         ((consp expression) (eval/combiner expression environment))
@@ -17,32 +20,31 @@
         (invoke f env (cdr expression))
         (error "Not an operator: ~A." f))))
 
-(defun lookup (symbol env)
-  (if (consp env)
-      (if (eq (caar env) symbol)
-          (cdar env)
-          (lookup symbol (cdr env)))
+(defun lookup (symbol &optional (env *global-env*))
+  (if env
+      (or (gethash symbol (env-bindings env))
+          (lookup symbol (env-parent env)))
       (error "No binding for ~A." symbol)))
 
 (defun (setf lookup) (new-value symbol env)
-  (if (consp env)
-      (if (eq (caar env) symbol)
-          (setf (cdr (car env)) new-value)
-          (setf (lookup symbol (cdr env)) new-value))
+  (if env
+      (if (gethash symbol (env-bindings env))
+          (setf (gethash symbol (env-bindings env)) new-value)
+          (setf (lookup symbol (env-parent env)) new-value))
       (error "No binding for ~A." symbol)))
 
-(defun extend (env variables values)
-  (cond ((consp variables)
-         (if (consp values)
-             (cons (cons (car variables) (car values))
-                   (extend env (cdr variables) (cdr values)))
-             (error "Not enough values.")))
-        ((null variables)
-         (if (null values)
-             env
-             (error "Too many values.")))
-        ((symbolp variables)
-         (cons (cons variables values) env))))
+(defun extend (env symbol value)
+  (if (nth-value 1 (gethash symbol (env-bindings env)))
+      (warn "Redefinition of ~A." symbol))
+  (setf (gethash symbol (env-bindings env)) value))
+
+(defun make-child-env (env variables values)
+  (make-env :parent env
+            :bindings (loop :for name :in variables
+                            :for binding :in values
+                            :with table = (make-hash-table :test 'eq)
+                            :do (setf (gethash name table) binding)
+                            :finally (return table))))
 
 (defvar *denv* nil)
 (defstruct clutter-operator function)
@@ -50,7 +52,7 @@
   (make-clutter-operator 
    :function
    (lambda (*denv* values)
-     (let ((env (extend env variables values)))
+     (let ((env (make-child-env env variables values)))
        (loop for sexp in body
           for last-value = (clutter-eval sexp env)
           finally (return last-value))))))
@@ -62,35 +64,40 @@
       (funcall (clutter-operator-function operator) env args)
       (error "Not a function: ~A." operator)))
 
-(defparameter *initial-env*
-  (list (cons '+ (make-clutter-operator
-                  :function (lambda (*denv* values)
-                              (reduce #'+ (mapcar (rcurry #'clutter-eval *denv*)
-                                                  values)))))
-        (cons 'car (make-clutter-operator
+(defparameter *global-env*
+  (make-env
+   :parent nil
+   :bindings
+   (alist-hash-table 
+    (list (cons '+ (make-clutter-operator
                     :function (lambda (*denv* values)
-                                (car (clutter-eval (car values) *denv*)))))
-        (cons 'eval (make-clutter-operator
-                     :function (lambda (*denv* values)
-                                 (let ((values (mapcar (rcurry #'clutter-eval *denv*)
-                                                       values)))
-                                   (clutter-eval (first values) (second values))))))
-        (cons 'get-current-env
-              (make-clutter-operator
-               :function (lambda (*denv* values)
-                           (declare (ignore values))
-                           *denv*)))
-        (cons 'vau
-              (make-clutter-operator
-               :function (lambda (static-env values)
-                           (destructuring-bind (env-var lambda-var &rest body)
-                               values
-                             (make-clutter-operator
-                              :function
-                              (lambda (*denv* values)
-                                (let ((env (extend static-env
-                                                   (list env-var lambda-var)
-                                                   (list *denv* values))))
-                                  (loop for sexp in body
-                                     for last-value = (clutter-eval sexp env)
-                                     finally (return last-value)))))))))))
+                                (reduce #'+ (mapcar (rcurry #'clutter-eval *denv*)
+                                                    values)))))
+          (cons 'car (make-clutter-operator
+                      :function (lambda (*denv* values)
+                                  (car (clutter-eval (car values) *denv*)))))
+          (cons 'eval (make-clutter-operator
+                       :function (lambda (*denv* values)
+                                   (let ((values (mapcar (rcurry #'clutter-eval *denv*)
+                                                         values)))
+                                     (clutter-eval (first values) (second values))))))
+          (cons 'get-current-env
+                (make-clutter-operator
+                 :function (lambda (*denv* values)
+                             (declare (ignore values))
+                             *denv*)))
+          (cons 'vau
+                (make-clutter-operator
+                 :function (lambda (static-env values)
+                             (destructuring-bind (env-var lambda-var &rest body)
+                                 values
+                               (make-clutter-operator
+                                :function
+                                (lambda (*denv* values)
+                                  (let ((env (make-child-env static-env
+                                                     (list env-var lambda-var)
+                                                     (list *denv* values))))
+                                    (loop for sexp in body
+                                          for last-value = (clutter-eval sexp env)
+                                          finally (return last-value))))))))))
+    :test 'eq)))
